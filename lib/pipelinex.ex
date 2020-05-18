9f771716fbf69d858c78e0d609c162bfc5949982
Kamil Lelonek <squixy.sln@gmail.com>
2020-05-18 15:37:06 +0200

    Test Pipelinex

--- lib/pipelinex.ex
+++ lib/pipelinex.ex
@@ -1,2 +1,21 @@
 defmodule Pipelinex do
+  defmacro __using__(_) do
+    quote do
+      require unquote(__MODULE__)
+
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro left ~> right do
+    quote do
+      case unquote(left) do
+        {:error, error} ->
+          {:error, error}
+
+        result ->
+          result |> unquote(right)
+      end
+    end
+  end
 end
--- test/pipelinex_test.exs
+++ test/pipelinex_test.exs
@@ -1,3 +1,31 @@
 defmodule PipelinexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Pipelinex
+
+  require Pipelinex
+
+  @text "Some random STRING."
+
+  describe "~>" do
+    test "should behave the same as the regular pipe operator" do
+      assert @text
+             |> String.downcase()
+             |> String.split() ==
+               @text
+               ~> String.downcase()
+               ~> String.split()
+    end
+
+    test "should handle error results" do
+      assert {:error, "invalid API key"} == "API KEY" ~> download() ~> parse()
+    end
+
+    test "should pass successful flow" do
+      assert %{downloaded_at: _timestamp, message: "valid result"} = 123 ~> download() ~> parse()
+    end
+
+    defp download(123), do: {:ok, "valid result"}
+    defp download(_), do: {:error, "invalid API key"}
+    defp parse({:ok, message}), do: %{message: message, downloaded_at: Time.utc_now()}
+  end
 end
